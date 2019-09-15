34f31caffa6eeccfa9a7f5ebb86db72778da60d7
League Student <league@iMac-27.attlocal.net>
2019-09-15 14:29:31 -0700

    Day 2 Part 2 of Level 0 Classes

---
+++ src/Unbirthday.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("When's your birthday?");
+		String birthday="September 22nd";
+		JOptionPane.showMessageDialog(null, "Wow, your bday is on "+birthday+"? Well then, Happy Birthday!");
+	}
+}
+
