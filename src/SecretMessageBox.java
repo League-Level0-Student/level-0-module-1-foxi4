c2db8721d1150926d85deb25b7df19f1800f2976
League Student <league@iMac-18.attlocal.net>
2019-10-13 14:29:26 -0700

    Day 6? of Java Class
    Hopefully the tomato proccesing thing saved!

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -6,16 +6,19 @@ public class SecretMessageBox {
 		String password="iwillgetthiswrong";
 		JOptionPane.showMessageDialog(null, "User, this is an important message! Please do not click out.");
 		JOptionPane.showMessageDialog(null, "You're still there? Good. Click OK.");
-		String answer=JOptionPane.showInputDialog("Now that you're listening, tell me your dark secret~~");
+		String answer=JOptionPane.showInputDialog("Now that you're listening, tell me your dark secret~");
 		JOptionPane.showMessageDialog(null, "Wow, who knew you kept something like this?");
 		JOptionPane.showMessageDialog(null, "Don't worry, your secret is safe with me. Now you can take your leave and let the second user come along.");
+		JOptionPane.showMessageDialog(null, "... has the User 1 left yet?");
+		JOptionPane.showMessageDialog(null, "... still making sure!");
 		JOptionPane.showMessageDialog(null, "Hello, user 2, today is your special day!");
 		JOptionPane.showMessageDialog(null, "You will get the chance to guess the first user's dark secret. (Sorry, user 1!)");
 		JOptionPane.showMessageDialog(null, "All you need to do is type the password.");
 		JOptionPane.showMessageDialog(null, "Yes, a password! Did you think it would be free?");
 		String attempt=JOptionPane.showInputDialog("Now, type your guess here. Make it count!");
-		if(attempt="iwillgetthiswrong")
+			JOptionPane.showMessageDialog(null,  "You got the password right! Here is user 1's dark secret: "+ answer +" :Yep, it's true!");
 		
 		}
 	}
 
+//SECRET: EVERY PASSWORD WORKS! SO TECHNICALLY USER 1'S SECRET WILL BE FOUND ANYWAY!
--- src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -3,8 +3,17 @@ void setup() {
 }
 void draw() {
     background(200, 200, 200);
+    background(#8BCB7D);
     noStroke();
+    fill(#F5393C);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
-    rect(176, 103, 12, 32);
+    fill(#3ADE5F);
+    rect(176, 103, 12, 32);}
+    { if (mousePressed){
+    fill(#8BCB7D);
+    ellipse(75,200,44,44);
+    ellipse(80,225,29,29);
+    
 }
+    }
