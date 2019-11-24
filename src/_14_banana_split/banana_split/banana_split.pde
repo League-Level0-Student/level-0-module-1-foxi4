a8ebc2d210c8e37a984389f09ef7fb157fd02f90
League Student <league@iMac-18.attlocal.net>
2019-11-24 14:22:44 -0800

    Finished Module 1!
    yay

---
+++ src/_14_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,12 @@
+
+void setup() {
+  size(200, 200);
+}
+void draw() {
+background(#CB2DC9);
+
+  for (int y = 80; y < 115; y+=15){ 
+text("ice cream", 75, y);
+}
+text("banana", 75, 125);
+}
--- src/badger_badger.java
+++ src/badger_badger.java
@@ -2,5 +2,21 @@
 public class badger_badger {
 	public static void main(String[] args) {
 		
+	for (int i = 0; i < 24; i++) {
+		
+		if(i<11){
+			System.out.println("badger");
+		
+		}
+		else if(i==11) {
+			System.out.println("mushroom");
+		}
+		else if(i<22) {
+			System.out.println("badger");
+		}
+		else {
+			System.out.println("snake");
+		}
+	}
 	}
 }
